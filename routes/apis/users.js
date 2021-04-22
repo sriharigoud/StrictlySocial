390416db83772980e080bdfca6b55e06cea0faa9
sriharigoud <srihari.goud2010@gmail.com>
2021-04-22 13:00:21 +0530

    reset password

--- routes/apis/users.js
+++ routes/apis/users.js
@@ -143,7 +143,7 @@ router.post(
         subject: "Password Reset",
         text: `You are receiving this because you (or someone else) have requested the reset of the password for your account.
           Please click on the following link, or paste this into your browser to complete the process:\n\n
-          http://${req.headers.host}/api/reset/${token}\n\n"
+          http://${req.headers.host}/reset/${token}\n\n"
           If you did not request this, please ignore this email and your password will remain unchanged.\n`,
       };
 
