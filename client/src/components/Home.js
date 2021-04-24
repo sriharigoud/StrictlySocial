7f9b1a71715af7d51bb44516078e6aecff12e9c8
sriharigoud <srihari.goud2010@gmail.com>
2021-04-24 10:23:30 +0530

    explor meny

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -106,7 +106,7 @@ export default function Home() {
   return (
     <div className="container-fluid mt-0 pt-2 gedf-wrapper border border-top-0 h-100">
       <div className="row">
-        <div className="col-md-3 d-none d-md-block">
+        <div className="col-md-3 ">
           <BasicInfo
             setUserInfo={() => console.log("Do nothing")}
             userInfo={currentUser}
